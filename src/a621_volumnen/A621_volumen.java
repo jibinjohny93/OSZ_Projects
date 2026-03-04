9d356ad770d86c5fb559051c1cce99b64f9818d5
JJW <JJW@JJW.sprintout.local>
2026-03-04 11:28:15 +0100

    Added A6.2.1 volume calculation

---
+++ src/a621_volumnen/A621_volumen.java
@@ -0,0 +1,22 @@
+package a621_volumnen;
+
+public class A621_volumen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        double erstesVolumen = volumenWuerfel(3.0);
+        System.out.println("Volumen von Wuerfel 1: " + erstesVolumen);
+        
+        double zweitesVolumen = volumenWuerfel(5.5);
+        System.out.println("Volumen von Wuerfel 2: " + zweitesVolumen);
+    }
+
+    public static double volumenWuerfel(double seitenlaenge) {
+        double volumen = seitenlaenge * seitenlaenge * seitenlaenge;
+        return volumen;
+		
+
+	}
+
+}
